d591b3a12a32be56ef6c2e5ee154719f2a700a2b
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2023-07-23 18:49:35 +0900

    0723
    졸림
    CpuParticle(title)
    move animation 2x
    tilemap(128px) proto

--- _Scripts/Player.gd
+++ _Scripts/Player.gd
@@ -4,9 +4,14 @@ extends CharacterBody2D
 const SPEED = 1500.0
 const JUMP_VELOCITY = -4000.0
 const EPSILON = 1
+const JUMP_ABILITY=3
+
+const BLEND_X = 50
+const BLEND_Y = 2000
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jumped=0
 
 var anim_tree : AnimationTree
 var anim_player : AnimationPlayer
@@ -19,11 +24,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		jumped=0
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and (is_on_floor() or jumped<JUMP_ABILITY):
 		velocity.y = JUMP_VELOCITY
-		
+		jumped+=1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -36,12 +43,12 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _process(delta):
-	anim_tree.set("parameters/blend_move_dir/blend_amount",clamp(velocity.x/50,-0.5,0.5)+0.5)
-	anim_tree.set("parameters/blend_move/blend_amount",clamp(abs(velocity.x/50),0,1))
+	anim_tree.set("parameters/blend_move_dir/blend_amount",clamp(velocity.x/BLEND_X,-0.5,0.5)+0.5)
+	anim_tree.set("parameters/blend_move/blend_amount",clamp(abs(velocity.x/BLEND_X),0,1))
 	if (velocity.y>0):
-		anim_tree.set("parameters/blend_fall/blend_amount",clamp(velocity.y/5000,0,1))
+		anim_tree.set("parameters/blend_fall/blend_amount",clamp(velocity.y/BLEND_Y,0,1))
 	else:
-		anim_tree.set("parameters/blend_jump/blend_amount",clamp(-velocity.y/5000,0,1))
+		anim_tree.set("parameters/blend_jump/blend_amount",clamp(-velocity.y/BLEND_Y,0,1))
 	if (velocity.y==0):
 		anim_tree.set("parameters/blend_fall/blend_amount",0)
 		anim_tree.set("parameters/blend_jump/blend_amount",0)
